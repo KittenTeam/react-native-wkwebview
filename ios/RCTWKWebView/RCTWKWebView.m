aba698a132e88dd575b2af83a3146b646c05f759
chengruilin <chengruilin@codemao.cn>
2018-11-26 16:01:06 +0800

    feat: change mediaTypesRequiringUserActionForPlayback

--- ios/RCTWKWebView/RCTWKWebView.m
+++ ios/RCTWKWebView/RCTWKWebView.m
@@ -64,6 +64,7 @@ RCT_NOT_IMPLEMENTED(- (instancetype)initWithCoder:(NSCoder *)aDecoder)
     _contentInset = UIEdgeInsetsZero;
 
     WKWebViewConfiguration* config = [[WKWebViewConfiguration alloc] init];
+    config.mediaTypesRequiringUserActionForPlayback = WKAudiovisualMediaTypeNone;
     config.processPool = processPool;
     WKUserContentController* userController = [[WKUserContentController alloc]init];
     [userController addScriptMessageHandler:[[WeakScriptMessageDelegate alloc] initWithDelegate:self] name:@"reactNative"];
